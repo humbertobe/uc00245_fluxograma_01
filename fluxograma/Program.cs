efbe4e7b4927ff875ec4815a79216f10643ec512
humbertobe <humbertobe@gmail.com>
2026-03-19 21:48:05 +0000

    fluxograma feito

---
+++ fluxograma/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace IndoParaTrabalho
+{
+     class Program
+    {
+        /* Variaveis*/
+        //variaveis chamadas de atributos
+        static void Main(string[] args) //variaveis dentro do (...) propriedades
+        {
+            Console.WriteLine("=== SEQUENCIA : INDO PARA O TRABALHO ===\n");
+            //inicio
+            Console.WriteLine("1. INICIO");
+            //sequaencia
+            Console.WriteLine("2. ACORDEI");
+            Console.WriteLine("3. VESTI");
+            Console.WriteLine("4. SAI DE CASA");
+            Console.WriteLine("5. ENTREI NO AUTOCARRO");
+            Console.WriteLine("6. ENTREI NO COMBOIO");
+            Console.WriteLine("7. CHEGUEI AO SERVIÇO");
+            //fim
+            Console.WriteLine("8. FIM\n");
+
+            Console.WriteLine("=== FIM DE SEQUENCIA ===\n");
+            
+        } //fim do metodo
+
+    } // fim de program   
+
+} // fim do namespace
